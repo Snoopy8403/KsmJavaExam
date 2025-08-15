e376c5ce54ac04b2c1b127641a728b344c22c397
Snoopy8403 <gomba.laszlo84@gmail.com>
2025-08-15 09:50:31 +0200

    Create the project

---
+++ src/test/java/com/ksmJavaExam/JavaExam/JavaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksmJavaExam.JavaExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
